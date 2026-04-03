8a692a95953dcb31cdb531118240bc64e1ae638b
kinh17631-star <kinh17631@gmail.com>
2026-04-04 00:40:12 +0530

    Update page.js

--- app/contact/page.js
+++ app/contact/page.js
@@ -2,8 +2,8 @@ export default function Contact() {
   return (
     <main className="p-10">
       <h1 className="text-3xl font-bold">Contact</h1>
-      <p className="mt-4 text-gray-400">Email: your@email.com</p>
-      <p className="text-gray-400">WhatsApp: +91XXXXXXXXXX</p>
+      <p className="mt-4 text-gray-400">Email: aarifsaifi4180@email.com</p>
+      <p className="text-gray-400">WhatsApp: +918650921106</p>
     </main>
   );
 }
