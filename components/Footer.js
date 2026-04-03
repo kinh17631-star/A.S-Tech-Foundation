66455fab7b752fc3e7ae223b029599fdccc02539
kinh17631-star <kinh17631@gmail.com>
2026-04-03 23:27:22 +0530

    Create Footer.js

---
+++ components/Footer.js
@@ -0,0 +1,12 @@
+export default function Footer() {
+  return (
+    <footer style={{
+      padding: "20px",
+      textAlign: "center",
+      borderTop: "1px solid #1e293b",
+      marginTop: "40px"
+    }}>
+      <p>© 2026 A.S Tech Foundation</p>
+    </footer>
+  );
+}
