5bd626b154bb92386de73e6aba2dfa3685feed9f
kinh17631-star <kinh17631@gmail.com>
2026-04-03 23:20:50 +0530

    Create page.js

---
+++ app/contact/page.js
@@ -0,0 +1,9 @@
+export default function Contact() {
+  return (
+    <main style={{ padding: "40px" }}>
+      <h1>Contact</h1>
+      <p>Email: aarifsaifi4180@email.com</p>
+      <p>WhatsApp: +918650921106</p>
+    </main>
+  );
+}
