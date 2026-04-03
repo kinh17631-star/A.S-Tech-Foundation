279ab4fb7894eeef0ad7bc23092e91f6c94e0b00
kinh17631-star <kinh17631@gmail.com>
2026-04-03 23:19:20 +0530

    Create page.js

---
+++ app/about/page.js
@@ -0,0 +1,8 @@
+export default function About() {
+  return (
+    <main style={{ padding: "40px" }}>
+      <h1>About Us</h1>
+      <p>We create and sell high-quality websites for businesses and creators.</p>
+    </main>
+  );
+}
