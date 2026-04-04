26326d7bf2a24e0ba8eaa6155b7f7d9707ce9006
kinh17631-star <kinh17631@gmail.com>
2026-04-04 12:21:00 +0530

    Update page.js

--- app/product/[id]/page.js
+++ app/product/[id]/page.js
@@ -81,7 +81,7 @@ export default function ProductPage({ params }) {
       </p>
 
       {/* WhatsApp Buy */}
-      <a href="https://wa.me/919876543210" target="_blank">
+      <a href="https://wa.me/918650921106" target="_blank">
         <button className="mt-6 px-6 py-3 bg-green-500 rounded">
           Buy on WhatsApp
         </button>
