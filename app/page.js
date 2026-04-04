197dfc289885439cba2a12e2e04ff6f935ba23a2
kinh17631-star <kinh17631@gmail.com>
2026-04-04 11:41:32 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,10 +1,12 @@
 import HeroSection from "../components/HeroSection";
 import CTASection from "../components/CTASection";
+import TrustSection from "../components/TrustSection";
 
 export default function Home() {
   return (
     <>
       <HeroSection />
+      <TrustSection />
       <CTASection />
     </>
   );
